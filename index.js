7a302be62f578494daf174929a1d11dbc9998106
zachjohnson <johnsza5@oregonstate.edu>
2023-05-16 14:44:19 -0700

    First Commit, Will edit from here

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
